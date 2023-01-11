5c591478b43f551452e09e141a42ab0ae760dd65
Appz-dk <patrick.fyhn@gmail.com>
2023-01-11 11:34:01 +0100

    adding dist

--- Trivia-app/src/api/getCategories.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-export const API_URL = "https://the-trivia-api.com/api"
-
-export const getCategories = async () => {
-    return await axios(`${API_URL}/categories`)
-}
